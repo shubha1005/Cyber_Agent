4ca329417fb17337a3f25fbc1177bbb591b7602d
shubhamsikakul <sikakulshubham10.jaihind@gmail.com>
2024-12-08 19:41:32 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Cryptography from "./Cryptography"
+import Port from "./Port"
+
+const App = () => {
+  return (
+    <div>
+      <Cryptography />
+      <Port />
+    </div>
+  );
+};
+
+export default App;
